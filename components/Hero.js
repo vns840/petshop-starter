b20e75614c6596362027577cf525bd99f6b6dd2e
Vinicius José <vinicius840_js@hotmail.com>
2023-08-02 17:49:34 -0300

    start of the project

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,9 @@
-import React from 'react';
-
-const Hero = () => {
-  return <div>Hero</div>;
-};
-
-export default Hero;
+import pretitleImg from '../public/img/hero/pretitle-img.svg';
+import Header from './Header';
+export default function Hero(){
+  return(
+    <section className='bg-crem lg:bg-hero lg:bg-cover lg:bg-no-repeat min-h-[400px] lg:min-h-[805px]'>
+      <Header />
+    </section>
+  )
+}
