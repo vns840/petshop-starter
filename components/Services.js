26391487ad0f759f3b1f2a4d7fe6b06cc7106464
Vinicius José <vinicius840_js@hotmail.com>
2023-08-04 07:33:38 -0300

    commit de add

--- components/Services.js
+++ components/Services.js
@@ -1,7 +1,7 @@
-import React from 'react';
-
-const Services = () => {
-  return <div>Services</div>;
-};
-
-export default Services;
+export default function Services(){
+  return(
+    <section className="bg-services bg-cover bg-no-repeat bg-center min-h-[862px] py-8">
+      
+    </section>
+  )
+}
