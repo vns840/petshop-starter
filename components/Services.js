5c65ba8657270f5a382e04bf1056e297d8a81c30
Vinicius José <vinicius840_js@hotmail.com>
2023-08-03 22:21:32 -0300

    services session ended

--- components/Services.js
+++ components/Services.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import ServiceSlider from './ServiceSlider'
 
-const Services = () => {
-  return <div>Services</div>;
-};
-
-export default Services;
+export default function Services(){
+  return(
+    <section className="bg-services bg-cover bg-no-repeat bg-center min-h-[862px] py-8">
+      <div className="container mx-auto h-[862px]">
+        <h2 className="h2 mb-12">Our Services</h2>
+        <ServiceSlider />
+      </div>
+    </section>
+  )
+}
